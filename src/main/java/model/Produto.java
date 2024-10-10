5f4a00ac70b41e098de3a63f7b19afb30ff1102b
damaresgaia <damaresgaia@gmail.com>
2024-10-10 15:30:27 -0300

    Criação do modelo Produto

--- src/main/java/model/Produto.java
+++ src/main/java/model/Produto.java
@@ -1,8 +1,49 @@
 package model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Produto {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String nome;
+	private Double preco;
+	private String descricao;
+	private String imagemUrl;  // URL da imagem do produto
 	
-
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getImagemUrl() {
+		return imagemUrl;
+	}
+	public void setImagemUrl(String imagemUrl) {
+		this.imagemUrl = imagemUrl;
+	}	
 }
