1ef4633928ba47bf7e81b5ee03e4d92a64dc4379
damaresgaia <damaresgaia@gmail.com>
2024-10-10 17:00:58 -0300

    Remoção da dependência Security

--- src/main/java/controller/ProdutoController.java
+++ src/main/java/controller/ProdutoController.java
@@ -1,5 +1,35 @@
 package controller;
 
-public class ProdutoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import model.Produto;
+import service.ProdutoService;
+
+@Controller
+@RequestMapping("/produtos")
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@GetMapping
+	public String listarProdutos(Model model) {
+		List<Produto> produtos = produtoService.listarProdutos();
+		model.addAttribute("produtos", produtos);
+		return "produtos/lista";
+	}
+	
+	@GetMapping("/detalhes")
+	public String detalhesProduto(@RequestParam Long id, Model model) {
+		Produto produto = produtoService.buscarPorId(id);
+		model.addAttribute("produto", produto);
+		return "produtos/detalhes";
+	}
 }
