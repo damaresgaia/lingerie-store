929415c4387a519d78c00915f3db2c3509b4e0b5
damaresgaia <damaresgaia@gmail.com>
2024-10-10 14:52:15 -0300

    Primeiro commit

---
+++ src/main/java/com/lingerie_store/LingerieStoreApplication.java
@@ -0,0 +1,13 @@
+package com.lingerie_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingerieStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingerieStoreApplication.class, args);
+	}
+
+}
