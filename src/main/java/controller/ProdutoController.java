675c0f3a60be028b68888cd20110dc1dd4cd3a67
damaresgaia <damaresgaia@gmail.com>
2024-10-14 20:58:41 -0300

    Templates

--- src/main/java/controller/ProdutoController.java
+++ src/main/java/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,4 +33,16 @@ public class ProdutoController {
 		model.addAttribute("produto", produto);
 		return "produtos/detalhes";
 	}
+	
+	@GetMapping("/adicionar")
+    public String mostrarFormularioAdicionarProduto(Model model) {
+        model.addAttribute("produto", new Produto());
+        return "produtos/adicionar"; // Nome da página Thymeleaf
+    }
+
+    @PostMapping("/adicionar")
+    public String adicionarProduto(Produto produto) {
+        produtoService.salvarProduto(produto);
+        return "redirect:/produtos"; // Redireciona após adicionar
+    }
 }
